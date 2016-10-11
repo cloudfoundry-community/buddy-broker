99238ab055b378d46916208846143524af422f1a
Dr Nic Williams <drnicwilliams@gmail.com>
2016-10-11 11:34:23 +1000

    migrate vendoring to vendor/ folder; update deps

---
+++ vendor/github.com/pivotal-cf/brokerapi/response.go
@@ -0,0 +1,30 @@
+package brokerapi
+
+type EmptyResponse struct{}
+
+type ErrorResponse struct {
+	Error       string `json:"error,omitempty"`
+	Description string `json:"description"`
+}
+
+type CatalogResponse struct {
+	Services []Service `json:"services"`
+}
+
+type ProvisioningResponse struct {
+	DashboardURL  string `json:"dashboard_url,omitempty"`
+	OperationData string `json:"operation,omitempty"`
+}
+
+type UpdateResponse struct {
+	OperationData string `json:"operation,omitempty"`
+}
+
+type DeprovisionResponse struct {
+	OperationData string `json:"operation,omitempty"`
+}
+
+type LastOperationResponse struct {
+	State       string `json:"state"`
+	Description string `json:"description,omitempty"`
+}
